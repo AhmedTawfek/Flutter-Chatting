31c784e53371ea4b9fee77cad4f567be182009cb
Updated to client_id <ahmed.tawfek32@gmail.com>
2025-02-20 01:52:57 +0200

    Add chat screen.

---
+++ lib/core/presentation/widgets_common/chat_message_bubble.dart
@@ -0,0 +1,54 @@
+import 'package:chatting/core/presentation/theming/color_manager.dart';
+import 'package:flutter/material.dart';
+
+import '../theming/styles.dart';
+
+class PrimaryChatMessageBubble extends StatelessWidget {
+  final String messageText;
+  final bool isSender;
+  final String? messageTime;
+
+  const PrimaryChatMessageBubble({
+    super.key,
+    required this.messageText,
+    required this.isSender,
+    this.messageTime,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            decoration: BoxDecoration(
+              color: isSender ? ColorManager.primary : ColorManager.background,
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                messageText,
+                style: TextStyles.heading4.copyWith(
+                  color: isSender ? ColorManager.onPrimary : ColorManager.black,
+                ),
+              ),
+            ),
+          ),
+          if (messageTime != null)
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                messageTime ?? '',
+                style: TextStyles.subtitle3,
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}

c6c81d511571267d412300e0346746c27dff7432
Updated to client_id <ahmed.tawfek32@gmail.com>
2025-03-26 23:51:16 +0200

    initial commit

--- lib/presentation/ui/chat/cubit/chat_cubit.dart
+++ lib/presentation/ui/chat/cubit/chat_cubit.dart
@@ -21,8 +21,8 @@ class ChatCubit extends Cubit<ChatState>{
 
     ChatMessageModel newMessage = ChatMessageModel(message: text, messageTimestamp: Timestamp.now(), senderId: 'userId1');
     final updatedMessages = List<ChatMessageModel>.from(state.messages)..add(newMessage);
-    chatRepo.
-    emit(state.copyWith(messages: updatedMessages, inputText: ''));
+
+    //emit(state.copyWith(messages: updatedMessages, inputText: ''));
   }
 
 
